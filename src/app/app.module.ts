333914c41381cc16f396637a06be23641c12c6d6
fazeel.arif <fazeel.arif@flsl.pk>
2024-02-06 00:03:11 +0500

    as per requirment code is functional and working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SelectModule } from './shared/components/select/select.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ApiInterceptor } from './interceptors/api-interceptors';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LayoutModule } from './layout/layout.module';
+import {  } from './features/currency-converter/component/currency-converter.component';
+import { ChartModule } from '@syncfusion/ej2-angular-charts';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SelectModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    ChartModule
   ],
-  providers: [],
+  providers: [ {
+    provide: HTTP_INTERCEPTORS,
+    useClass: ApiInterceptor,
+    multi: true,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
